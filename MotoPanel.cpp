cd6323114a02def33c5ba5a87cd74f6fa98ee38c
Greg Green <gpgreen@gmail.com>
2017-08-19 15:25:49 -0700

    Fixed formatting

--- MotoPanel.cpp
+++ MotoPanel.cpp
@@ -7,9 +7,9 @@
 //============================================================================
 
 #if defined(ARDUINO) && ARDUINO >= 100
-  #include "Arduino.h"
+#include "Arduino.h"
 #else
-  #include "WProgram.h"
+#include "WProgram.h"
 #endif
 
 #include "MotoPanel.h"
@@ -39,82 +39,82 @@ bool MotoPanel::update()
 
 void MotoPanel::drawSpeed(int spd)
 {
-  if (spd == _last_speed)
-    return;
-  _last_speed = spd;
+    if (spd == _last_speed)
+	return;
+    _last_speed = spd;
 
-  // blank speed
-  _display.fillRect(24, 10, 5*2*4, 10*4, WHITE);
+    // blank speed
+    _display.fillRect(24, 10, 5*2*4, 10*4, WHITE);
   
-  _display.setTextSize(4);
-  _display.setTextColor(BLACK);
-  _display.setCursor(24, 10);
-  _display.print(spd, DEC);
+    _display.setTextSize(4);
+    _display.setTextColor(BLACK);
+    _display.setCursor(24, 10);
+    _display.print(spd, DEC);
 
-  _update_display = true;
+    _update_display = true;
 }
 
 void MotoPanel::drawMileage(int mileage)
 {
-  if (_last_mileage == mileage)
-    return;
-  _last_mileage = mileage;
+    if (_last_mileage == mileage)
+	return;
+    _last_mileage = mileage;
 
-  // blank mileage
-  _display.fillRect(24, 40, 5*8, 10, WHITE);
+    // blank mileage
+    _display.fillRect(24, 40, 5*8, 10, WHITE);
 
-  _display.setTextSize(1);
-  _display.setTextColor(BLACK);
-  _display.setCursor(24, 40);
+    _display.setTextSize(1);
+    _display.setTextColor(BLACK);
+    _display.setCursor(24, 40);
 
-  int x = mileage;
-  int digit = mileage / 1000000;
-  x -= digit * 1000000;
-  _display.print(digit, DEC);
-  digit = x / 100000;
-  x -= digit * 100000;
-  _display.print(digit, DEC);
-  digit = x / 10000;
-  x -= digit * 10000;
-  _display.print(digit, DEC);
-  digit = x / 1000;
-  x -= digit * 1000;
-  _display.print(digit, DEC);
-  digit = x / 100;
-  x -= digit * 100;
-  _display.print(digit, DEC);
-  digit = x / 10;
-  x -= digit * 10;
-  _display.print(digit, DEC);
-  _display.print(".");
-  _display.print(x, DEC);
+    int x = mileage;
+    int digit = mileage / 1000000;
+    x -= digit * 1000000;
+    _display.print(digit, DEC);
+    digit = x / 100000;
+    x -= digit * 100000;
+    _display.print(digit, DEC);
+    digit = x / 10000;
+    x -= digit * 10000;
+    _display.print(digit, DEC);
+    digit = x / 1000;
+    x -= digit * 1000;
+    _display.print(digit, DEC);
+    digit = x / 100;
+    x -= digit * 100;
+    _display.print(digit, DEC);
+    digit = x / 10;
+    x -= digit * 10;
+    _display.print(digit, DEC);
+    _display.print(".");
+    _display.print(x, DEC);
 
-  _update_display = true;
+    _update_display = true;
 }
 
 void MotoPanel::drawRPM(int rpm)
 {
-  if (_last_rpm == rpm)
-    return;
-  _last_rpm = rpm;
+    if (_last_rpm == rpm)
+	return;
+    _last_rpm = rpm;
 
-  // blank rpm
-  _display.fillRect(0, 12, 10, 47, WHITE);
-  _display.fillRect(1, 1, 5*6, 10, WHITE);
+    // blank rpm
+    _display.fillRect(0, 12, 10, 47, WHITE);
+    _display.fillRect(1, 1, 5*6, 10, WHITE);
   
-  int delta = rpm / (_rpm_range/(47 - 12));
-  int triy = 47 - delta;
-  int trix = rpm / (_rpm_range/10);
-  _display.fillTriangle(0, 47, 0, triy, trix, triy, BLACK);
-  _display.drawLine(0, triy, 0, 12, BLACK);
-  _display.drawLine(0, 12, 10, 12, BLACK);
-  _display.drawLine(10, 12, trix, triy, BLACK); 
+    int delta = rpm / (_rpm_range/(47 - 12));
+    int triy = 47 - delta;
+    int trix = rpm / (_rpm_range/10);
+    _display.fillTriangle(0, 47, 0, triy, trix, triy, BLACK);
+    _display.drawLine(0, triy, 0, 12, BLACK);
+    _display.drawLine(0, 12, 10, 12, BLACK);
+    _display.drawLine(10, 12, trix, triy, BLACK); 
   
-  _display.setTextSize(1);
-  _display.setTextColor(BLACK);
-  _display.setCursor(1, 1);
-  _display.print(rpm, DEC);
+    _display.setTextSize(1);
+    _display.setTextColor(BLACK);
+    _display.setCursor(1, 1);
+    _display.print(rpm, DEC);
 
-  _update_display = true;
+    _update_display = true;
 }
 
--- MotoPanel.h
+++ MotoPanel.h
@@ -14,27 +14,27 @@
 class MotoPanel
 {
 public:
-  explicit MotoPanel(Adafruit_GFX& display);
+    explicit MotoPanel(Adafruit_GFX& display);
 
-  // initialize all state
-  void begin(int rpm_range);
+    // initialize all state
+    void begin(int rpm_range);
 
-  // send values to panel to draw
-  void drawSpeed(int spd);
-  void drawMileage(int mileage);
-  void drawRPM(int rpm);
+    // send values to panel to draw
+    void drawSpeed(int spd);
+    void drawMileage(int mileage);
+    void drawRPM(int rpm);
 
-  // returns true if panel needs a display update, false if not
-  bool update();
+    // returns true if panel needs a display update, false if not
+    bool update();
   
 private:
 
-  Adafruit_GFX& _display;
-  int _last_speed;
-  int _last_mileage;
-  int _last_rpm;
-  int _rpm_range;
-  bool _update_display;
+    Adafruit_GFX& _display;
+    int _last_speed;
+    int _last_mileage;
+    int _last_rpm;
+    int _rpm_range;
+    bool _update_display;
 };
 
 #endif /* MOTOPANEL_H_ */
